b013e362345c264d3bf22602d1084b4261530cde
ddux <dusan.stanojevic.cs@gmail.com>
2026-02-22 12:30:34 -0600

    bug: fixed incorrect table naming from lit

--- backend/app/models/models.go
+++ backend/app/models/models.go
@@ -6,6 +6,12 @@ import (
 
 var ExtensionModelRegistrations []func()
 
+type metricRegistryNaming struct{ lit.DefaultDbNamingStrategy }
+
+func (metricRegistryNaming) GetTableNameFromStructName(string) string {
+	return "metric_registry"
+}
+
 func Init() {
 	lit.RegisterModel[Project](lit.PostgreSQL)
 	lit.RegisterModel[User](lit.PostgreSQL)
@@ -17,7 +23,7 @@ func Init() {
 	lit.RegisterModel[UserOrganizationResponse](lit.PostgreSQL)
 	lit.RegisterModel[CountResult](lit.PostgreSQL)
 	lit.RegisterModel[SourceMap](lit.PostgreSQL)
-	lit.RegisterModel[MetricRegistry](lit.PostgreSQL)
+	lit.RegisterModelWithNaming[MetricRegistry](lit.PostgreSQL, metricRegistryNaming{})
 	lit.RegisterModel[WidgetGroup](lit.PostgreSQL)
 	lit.RegisterModel[WidgetGroupWidget](lit.PostgreSQL)
 
