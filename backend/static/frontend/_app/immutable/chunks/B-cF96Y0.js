a1e33dd9c5855f8b8b69e99e80bf808aa6f6cdad
ddux <dusan.stanojevic.cs@gmail.com>
2026-04-26 23:08:17 -0500

    release: backend v1.7.10

--- backend/static/frontend/_app/immutable/chunks/B-cF96Y0.js
+++
@@ -1,1 +0,0 @@
-import{R as e}from"./Csm2D17K.js";function i(r,t){throw new e(r,t.toString())}export{i as r};
--- backend/static/frontend/_app/immutable/entry/start.Cdg4TLfL.js
+++
@@ -1,1 +0,0 @@
-import{l as o,c as r}from"../chunks/Csm2D17K.js";export{o as load_css,r as start};
